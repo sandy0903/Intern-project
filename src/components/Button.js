0c19b3de4e5d22c46c4234def23419a038bad7ad
duyen huynh <duyen.huynh@alpaca.vn>
2022-07-25 22:37:28 +0700

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Button=({bgColor, text})=>{
+    return <div className='bgColor btn'>
+        {text}
+
+    </div>
+}
+export default Button
