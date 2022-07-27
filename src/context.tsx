81bf1ff76e8fb602ab828da9bea45fe2644ab70d
duyen huynh <duyen.huynh@alpaca.vn>
2022-07-27 11:39:33 +0700

    delete state needless

--- src/context.tsx
+++ src/context.tsx
@@ -2,16 +2,10 @@ import React, { SetStateAction, useContext, useState } from "react";
 
 export type ContextProp = {
   clearValue: boolean;
-  key?: any;
-  number?: number | null | string|[];
-  result?:any;
-  value?: any[];
-  display?: string,
+  number?: number | null | string | [];
+  display: string;
   handleKeyBoard: (e: any) => any;
-  calculatorResult?: () => any;
   setClearValue?: React.Dispatch<React.SetStateAction<boolean>>;
-  setKey?: React.Dispatch<React.SetStateAction<string | null | number>>;
-  setValue?: React.Dispatch<React.SetStateAction<any[]>>;
   setDisplay?: React.Dispatch<React.SetStateAction<string>>;
 };
 const AppContext = React.createContext<ContextProp | null>(null);
@@ -19,90 +13,74 @@ const AppContext = React.createContext<ContextProp | null>(null);
 const AppProvider = ({ children }: any) => {
   // clear previous syntax or value
   const [clearValue, setClearValue] = useState(true);
-
-  const [showResult, setResult]=useState<number>(0)
-
-  // key: +/- % / x - +
-  const [key, setKey] = useState<SetStateAction<string | null | number>>(null);
-  const [syntax, setSyntax] = useState<string | null|[]>(null);
   const [number, setNumber] = useState<number | string | null>(null);
-  const [value, setValue] = useState<any[]>([]);
-const [display, setDisplay]=useState<string>("")
+  const [display, setDisplay] = useState<string>("");
+
 
   const handleKeyBoard = (value: any) => {
-    console.log(value)
-    // set pre key
-    if(value !== "AC" && value !== "C" && value!=="=" && value!=="+/-" && value!=="%"){
-        setKey(value)
-    }
     if (typeof value == "number") {
       // if pre value is number => clear number
       setClearValue(false);
 
-    //   set number
+      //   set number
       if (number) {
-
         setNumber((pre) => pre + value.toString());
-
         setDisplay((pre) => pre + value.toString());
-
-      } else {
+      } else{
         setNumber(value.toString());
         setDisplay(value.toString());
       }
     }
 
-    // set syntax
-    else if (typeof value !== "number" && value!=="=" && value!=="+/-" && value!=="%")  {
-      setSyntax(value);
-
-      if(number){
+    else if (
+      typeof value !== "number" &&
+      value !== "=" &&
+      value !== "+/-" &&
+      value !== "%"
+    ) {
+      if (number) {
         setDisplay((pre) => pre + value.toString());
-        setClearValue(false);
-      }else{
-        setClearValue(true)
-      }
 
+        setClearValue(false);
+      } else {
+        setClearValue(true);
+      }
     }
 
     switch (value) {
       case "C":
         setClearValue(true);
-        setKey(null);
-        setNumber(0)
-        setDisplay("")
+        setNumber(0);
+        setDisplay("");
         break;
       case "AC":
         setNumber(0);
-        setDisplay("")
+        setDisplay("");
 
         break;
       case "+/-":
         setDisplay((pre) =>
-
           typeof pre == "string" ? (parseInt(pre) * -1).toString() : pre
         );
 
         break;
       case "%":
-
         setDisplay((pre) =>
           typeof pre == "string" ? (parseInt(pre) / 100).toString() : pre
         );
 
         break;
       case "=":
-        setDisplay(eval(display).toString())
-        // setDisplay(pre=>typeof pre=='string' && eval(pre))
+        console.log(eval(display.replaceAll(",",".")).toString().length)
+        setDisplay(eval(display.replaceAll(",",".")).toString().length>3 ? eval(display.replaceAll(",",".")).toFixed(1).toString() : eval(display.replaceAll(",",".")).toString() );
         break;
-
     }
-
   };
 
-
   return (
-    <AppContext.Provider value={{ clearValue, handleKeyBoard, number,display }}>
+    <AppContext.Provider
+      value={{ clearValue, handleKeyBoard, display }}
+    >
       {children}
     </AppContext.Provider>
   );
